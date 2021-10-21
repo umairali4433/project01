04bd822deee97b4b288470d6d38d46fcca42463d
umiiali4433@gmail.com <Umairali100>
2021-10-21 15:31:48 +0500

    adasdas

--- lib/homscreen/body.dart
+++ lib/homscreen/body.dart
@@ -20,222 +20,247 @@ class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey.shade900,
+      appBar: AppBar(
+          backgroundColor: Colors.grey.shade900,
+          elevation: 0,
+      ),
+      drawer:Container(width:300,color:Colors.white,child: ListView(children:
+      [
+        SizedBox(height: 200,),
+        Divider(height: 10,),
+        Padding(
+          padding: const EdgeInsets.only(left: 8.0),
+          child: Text('Profie'),
 
-
-      body: Container(
-        decoration: new BoxDecoration(
-            color: Colors.black45
         ),
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-          Center(child: Image.asset('assets/images/divider.png')),
-              SizedBox(height: 20,),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                child: Card(
-                  child: Container(
-                    height: 150,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(4.0),
-                      color: Colors.indigo,
-                    ),
-                    child: Swiper(
-                      itemCount: imageList.length,
-                      itemBuilder: (context, index) {
-                        return Row(
-                          children: <Widget>[
+        Divider(height: 10,)
 
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: <Widget>[
+      ],)),
 
-                            Expanded(
-                              child: Container(
-                                width:MediaQuery.of(context).size.width*0.90,
-                                height: 200,
-                                child: Image.network(
-                                imageList[index],
-                                  fit: BoxFit.cover,
-                                 ),
-                              ),
-                            )
-
-                              ],
-                            ),
-
-                          ],
-                        );
-                      },
-                    ),
-                  ),
-                ),
-              ),
-
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                child: Row(
-                  children: <Widget>[
-                    Text(
-                      "Photos"
-                    ,style: TextStyle(color: Colors.black,fontSize: 22)
-                    ),
-                    Spacer(),
-                    FlatButton(
-                      child: Text("...",style: TextStyle(color: Colors.black,fontSize: 30),),
-                      onPressed: () {},
-                    ),
-                  ],
-                ),
-
-              ),
-
-              Padding(
-                padding: const EdgeInsets.only(left: 8.0),
-                child: SizedBox(
-                  height: 200,
-                  child: ListView(
-                    shrinkWrap: true,
-                    physics: ClampingScrollPhysics(),
-                    // This next line does the trick.
-                    scrollDirection: Axis.horizontal,
-                    children: <Widget>[
-                      Container(
-                        width: 150.0,
-                        height: 250.0,
-                        decoration: BoxDecoration(
-                          image: DecorationImage(
-                              fit: BoxFit.cover, image: NetworkImage( imageList[0])),
-                          borderRadius: BorderRadius.all(Radius.circular(8.0)),
-                          color: Colors.redAccent,
-                        ),
-                      ),
-                      SizedBox(width: 20,),
-                      Container(
-                        width: 150.0,
-                        height: 250.0,
-                        decoration: BoxDecoration(
-                          image: DecorationImage(
-                              fit: BoxFit.cover, image: NetworkImage( imageList[1])),
-                          borderRadius: BorderRadius.all(Radius.circular(8.0)),
-                          color: Colors.redAccent,
-                        ),
-                      ),
-                      SizedBox(width: 20,),
-                      Container(
-                        width: 150.0,
-                        height: 250.0,
-                        decoration: BoxDecoration(
-                          image: DecorationImage(
-                              fit: BoxFit.cover, image: NetworkImage( imageList[2])),
-                          borderRadius: BorderRadius.all(Radius.circular(8.0)),
-                          color: Colors.redAccent,
-                        ),
-                      ),
-                      SizedBox(width: 20,),
-                      Container(
-                        width: 150.0,
-                        height: 250.0,
-                        decoration: BoxDecoration(
-                          image: DecorationImage(
-                              fit: BoxFit.cover, image: NetworkImage( imageList[3])),
-                          borderRadius: BorderRadius.all(Radius.circular(8.0)),
-                          color: Colors.redAccent,
-                        ),
-                      ),
-
-
-
-
-
-                    ],
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                child: Row(
-                  children: <Widget>[
-                    Text(
-                        "Videos"
-                        ,style: TextStyle(color: Colors.black,fontSize: 22)
-                    ),
-                    Spacer(),
-                    FlatButton(
-                      child: Text("...",style: TextStyle(color: Colors.black,fontSize: 30),),
-                      onPressed: () {},
-                    ),
-                  ],
-                ),
-
-              ),
-
-              Padding(
-                padding: const EdgeInsets.only(left: 8.0),
-                child: SizedBox(
-                  height: 200,
-                  child: ListView(
-                    shrinkWrap: true,
-                    physics: ClampingScrollPhysics(),
-                    // This next line does the trick.
-                    scrollDirection: Axis.horizontal,
-                    children: <Widget>[
-                      Container(
-                        width: 150.0,
-                        height: 250.0,
-                        decoration: BoxDecoration(
-                          image: DecorationImage(
-                              fit: BoxFit.cover, image: NetworkImage( imageList[0])),
-                          borderRadius: BorderRadius.all(Radius.circular(8.0)),
-                          color: Colors.redAccent,
-                        ),
-                      ),
-                      SizedBox(width: 20,),
-                      Container(
-                        width: 150.0,
-                        height: 250.0,
-                        decoration: BoxDecoration(
-                          image: DecorationImage(
-                              fit: BoxFit.cover, image: NetworkImage( imageList[1])),
-                          borderRadius: BorderRadius.all(Radius.circular(8.0)),
-                          color: Colors.redAccent,
-                        ),
-                      ),
-                      SizedBox(width: 20,),
-                      Container(
-                        width: 150.0,
-                        height: 250.0,
-                        decoration: BoxDecoration(
-                          image: DecorationImage(
-                              fit: BoxFit.cover, image: NetworkImage( imageList[2])),
-                          borderRadius: BorderRadius.all(Radius.circular(8.0)),
-                          color: Colors.redAccent,
-                        ),
-                      ),
-                      SizedBox(width: 20,),
-                      Container(
-                        width: 150.0,
-                        height: 250.0,
-                        decoration: BoxDecoration(
-                          image: DecorationImage(
-                              fit: BoxFit.cover, image: NetworkImage( imageList[3])),
-                          borderRadius: BorderRadius.all(Radius.circular(8.0)),
-                          color: Colors.redAccent,
-                        ),
-                      ),
-
-
-
-
-
-                    ],
-                  ),
-                ),
-              )
-            ],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: 0, // this will be set when a new tab is tapped
+        items: [
+          BottomNavigationBarItem(
+            icon: new Icon(Icons.home),
+            title: new Text('Home'),
           ),
+          BottomNavigationBarItem(
+            icon: new Icon(Icons.mail),
+            title: new Text('Messages'),
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              title: Text('Profile')
+          )
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+        Center(child: Image.asset('assets/images/divider.png')),
+            SizedBox(height: 20,),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Card(
+                child: Container(
+                  height: 150,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(4.0),
+                    color: Colors.indigo,
+                  ),
+                  child: Swiper(
+                    itemCount: imageList.length,
+                    itemBuilder: (context, index) {
+                      return Row(
+                        children: <Widget>[
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                          Expanded(
+                            child: Container(
+                              width:MediaQuery.of(context).size.width*0.90,
+                              height: 200,
+                              child: Image.network(
+                              imageList[index],
+                                fit: BoxFit.cover,
+                               ),
+                            ),
+                          )
+
+                            ],
+                          ),
+
+                        ],
+                      );
+                    },
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Row(
+                children: <Widget>[
+                  Text(
+                    "Photos"
+                  ,style: TextStyle(color: Colors.white,fontSize: 22)
+                  ),
+                  Spacer(),
+                  FlatButton(
+                    child: Text("...",style: TextStyle(color: Colors.white,fontSize: 30),),
+                    onPressed: () {},
+                  ),
+                ],
+              ),
+
+            ),
+
+            Padding(
+              padding: const EdgeInsets.only(left: 8.0),
+              child: SizedBox(
+                height: 200,
+                child: ListView(
+                  shrinkWrap: true,
+                  physics: ClampingScrollPhysics(),
+                  // This next line does the trick.
+                  scrollDirection: Axis.horizontal,
+                  children: <Widget>[
+                    Container(
+                      width: 150.0,
+                      height: 250.0,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                            fit: BoxFit.cover, image: NetworkImage( imageList[0])),
+                        borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                        color: Colors.redAccent,
+                      ),
+                    ),
+                    SizedBox(width: 20,),
+                    Container(
+                      width: 150.0,
+                      height: 250.0,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                            fit: BoxFit.cover, image: NetworkImage( imageList[1])),
+                        borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                        color: Colors.redAccent,
+                      ),
+                    ),
+                    SizedBox(width: 20,),
+                    Container(
+                      width: 150.0,
+                      height: 250.0,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                            fit: BoxFit.cover, image: NetworkImage( imageList[2])),
+                        borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                        color: Colors.redAccent,
+                      ),
+                    ),
+                    SizedBox(width: 20,),
+                    Container(
+                      width: 150.0,
+                      height: 250.0,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                            fit: BoxFit.cover, image: NetworkImage( imageList[3])),
+                        borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                        color: Colors.redAccent,
+                      ),
+                    ),
+
+
+
+
+
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Row(
+                children: <Widget>[
+                  Text(
+                      "Videos"
+                      ,style: TextStyle(color: Colors.white,fontSize: 22)
+                  ),
+                  Spacer(),
+                  FlatButton(
+                    child: Text("...",style: TextStyle(color: Colors.white,fontSize: 30),),
+                    onPressed: () {},
+                  ),
+                ],
+              ),
+
+            ),
+
+            Padding(
+              padding: const EdgeInsets.only(left: 8.0),
+              child: SizedBox(
+                height: 200,
+                child: ListView(
+                  shrinkWrap: true,
+                  physics: ClampingScrollPhysics(),
+                  // This next line does the trick.
+                  scrollDirection: Axis.horizontal,
+                  children: <Widget>[
+                    Container(
+                      width: 150.0,
+                      height: 250.0,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                            fit: BoxFit.cover, image: NetworkImage( imageList[0])),
+                        borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                        color: Colors.redAccent,
+                      ),
+                    ),
+                    SizedBox(width: 20,),
+                    Container(
+                      width: 150.0,
+                      height: 250.0,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                            fit: BoxFit.cover, image: NetworkImage( imageList[1])),
+                        borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                        color: Colors.redAccent,
+                      ),
+                    ),
+                    SizedBox(width: 20,),
+                    Container(
+                      width: 150.0,
+                      height: 250.0,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                            fit: BoxFit.cover, image: NetworkImage( imageList[2])),
+                        borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                        color: Colors.redAccent,
+                      ),
+                    ),
+                    SizedBox(width: 20,),
+                    Container(
+                      width: 150.0,
+                      height: 250.0,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                            fit: BoxFit.cover, image: NetworkImage( imageList[3])),
+                        borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                        color: Colors.redAccent,
+                      ),
+                    ),
+
+
+
+
+
+                  ],
+                ),
+              ),
+            )
+          ],
         ),
       ),
     );
